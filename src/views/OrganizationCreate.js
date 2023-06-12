3a4a033d7b69ddf9ceec221f41773047b0576c2b
Jorge Palacios <jorge.p.codes@gmail.com>
2023-06-12 17:44:28 -0400

    Implemented adding new organization

--- src/views/OrganizationCreate.js
+++ src/views/OrganizationCreate.js
@@ -1,27 +1,60 @@
 import React from 'react';
+import { useState } from 'react';
 
 const OrganizationCreate = () => {
-  const handleCreate = (evt) => {
-    evt.preventDefault();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [country, setCountry] = useState('');
+
+  const handleCreate = (e) => {
+    e.preventDefault();
     console.log('adding new organization...');
+    const organization = { name, address, country };
+    createOrganization(organization);
+  };
+
+  const createOrganization = (organization) => {
+    console.log(organization);
+
+    setIsLoading(true);
+
+    const url = 'http://localhost:8080/organizations';
+    fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(organization),
+    }).then(() => {
+      setName('');
+      setAddress('');
+      setCountry('');
+      setIsLoading(false);
+    });
   };
 
   return (
     <section className='organization-create'>
       <h2>Create new organization</h2>
-      <form>
+      <form onSubmit={handleCreate}>
         <div>
           <label htmlFor='name'>Name</label>
-          <input name='name' type='text' />
+          <input name='name' type='text' value={name} onChange={(e) => setName(e.target.value)} />
         </div>
         <div>
           <label htmlFor='address'>Address:</label>
-          <input name='address' type='text' />
+          <input name='address' type='text' value={address} onChange={(e) => setAddress(e.target.value)} />
         </div>
         <div>
-          <button onClick={handleCreate} type='submit'>
-            Add
-          </button>
+          <label htmlFor='country'>Country:</label>
+          <select name='country' value={country} onChange={(e) => setCountry(e.target.value)}>
+            <option value='US'>United States</option>
+            <option value='CL'>Chile</option>
+          </select>
+        </div>
+        <div>
+          {!isLoading && <button type='submit'>Add {name && `"${name}"`}</button>}
+          {isLoading && <button disabled>Adding...</button>}
         </div>
       </form>
     </section>
