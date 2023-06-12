a9388aa9e705fc77b7791fbab4e17c7f72b187a9
Jorge Palacios <jorge.p.codes@gmail.com>
2023-06-12 12:45:00 -0400

    Implemented loading and error messages

--- src/App.js
+++ src/App.js
@@ -3,17 +3,32 @@ import CategoryList from './components/CategoryList';
 
 function App() {
   const [categories, setCategories] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
+    requestCategories();
+  }, []);
+
+  const requestCategories = () => {
     const url = 'http://localhost:8080/categories';
+    setIsError(false);
+    setIsLoading(true);
+
     fetch(url)
       .then((res) => {
         return res.json();
       })
       .then((data) => {
+        setIsLoading(false);
         setCategories(data);
+      })
+      .catch((err) => {
+        console.error(err.message);
+        setIsError(true);
+        setIsLoading(false);
       });
-  }, []);
+  };
 
   const deleteCategory = (id) => {
     const newCategories = categories.filter((category) => category.id !== id);
@@ -23,6 +38,8 @@ function App() {
   return (
     <div className='App'>
       <h1>Trivia Dashboard</h1>
+      {isLoading && <h4>Loading...</h4>}
+      {isError && <h4>There was an error! Try again</h4>}
       {categories && <CategoryList title='Categories' categories={categories} deleteHandler={deleteCategory} />}
     </div>
   );
