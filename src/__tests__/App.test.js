701ac7b7c5c1ea15bd1b0ea9b849b2b700bc7db1
Jorge Palacios <jorge.p.codes@gmail.com>
2023-06-14 15:25:57 -0400

    Rearranged tests directory

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const main = screen.getByTestId('mainpoint');
+  const main = screen.getByTestId('main');
   expect(main).toBeInTheDocument();
 });
